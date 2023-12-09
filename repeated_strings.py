670b91426d4930a10dcbcb908d0775ba16104f53
149036 <sqrt2c@gmail.com>
2023-12-09 15:07:58 +0900

    TODO

--- repeated_strings.py
+++ repeated_strings.py
@@ -2,8 +2,24 @@ import numpy
 
 
 def repeated_strings(strings: str, horizontal: int, vertical: int):
+    """文字列を行と列に繰り返したものを返す
+
+    Args:
+        strings (str): 表示する文字列
+        horizontal (int): 列方向に文字列を表示する数
+        vertical (int): 行方向に文字列を繰り返す数
+
+    Raises:
+        Exception: 文字列が 5文字以上 10文字以下 でない場合エラー
+        Exception: 文字列に a-zA-Z 以外の物が入っていたらエラー
+
+    Returns:
+        str: result, 文字列を行と列に繰り返したもの
+    """
     if not (5 <= len(strings) <= 10):
-        raise Exception("5文字以上10文字以下")
+        # TODO: 一時的にprint
+        # raise Exception("5文字以上10文字以下")
+        print("5文字以上10文字以下")
 
     for char in strings:
         if not char.isalpha():
@@ -16,7 +32,6 @@ def repeated_strings(strings: str, horizontal: int, vertical: int):
 
     for i in range(vertical):
         result += strings + "\n"
-
     return result
 
 
