93aadbcc51803f4d32417c82380ecb0f65e67439
149036 <sqrt2c@gmail.com>
2023-12-09 14:53:56 +0900

    numpy

--- repeated_strings.py
+++ repeated_strings.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 def repeated_strings(strings: str, horizontal: int, vertical: int):
     if not (5 <= len(strings) <= 10):
         raise Exception("5文字以上10文字以下")
@@ -17,4 +20,8 @@ def repeated_strings(strings: str, horizontal: int, vertical: int):
     return result
 
 
-print(repeated_strings("sdfasadf", 5, 5))
+print(
+    repeated_strings(
+        "sdfasadf", numpy.random.randint(1, 10), numpy.random.randint(1, 10)
+    )
+)
